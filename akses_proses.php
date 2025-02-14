8cf53d2fae28685e1eff4922329b9f45ad76c18d
suwardy87 <suwardyser87@gmail.com>
2025-02-14 09:11:20 +0700

    erorr cek

--- akses_proses.php
+++ akses_proses.php
@@ -1,6 +1,6 @@
 <?php
 
-include('cek.php');
+// include('cek.php');
 include('config.php');
 include('fungsi.php');
 
--- bobot_hasil.php
+++ bobot_hasil.php
@@ -1,4 +1,5 @@
 <?php
+	// include('cek.php');
 	include('header.php');
 	
 ?>
